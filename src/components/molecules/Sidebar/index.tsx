6e79bfcae06a7aa83c3bbab6cf8da0b208c55786
Tatiana Sanchez <tatiana.sanchez.sanin@gmail.com>
2024-10-26 12:41:43 -0500

    refactor: :art: change sidebar items and styling

--- src/components/molecules/Sidebar/index.tsx
+++ src/components/molecules/Sidebar/index.tsx
@@ -1,21 +1,45 @@
 import React from 'react';
 import Link from 'next/link';
-import { Bell, Home, LineChart, Package, Package2, ShoppingCart, Users } from 'lucide-react';
-import { Avatar, AvatarFallback, AvatarImage } from '@/src/components/ui/avatar';
+import { useSession } from 'next-auth/react';
+
+import {
+  Bell,
+  Home,
+  LineChart,
+  BookCopy,
+  Library,
+  LibraryBig,
+  Users,
+} from 'lucide-react';
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from '@/src/components/ui/avatar';
 import { Badge } from '@/src/components/ui/badge';
 import { Button } from '@/src/components/ui/button';
-import { Card, CardContent, CardHeader, CardTitle } from '@/src/components/ui/card';
-import { useSession } from 'next-auth/react';
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from '@/src/components/ui/card';
+import { useRouter } from 'next/router';
 
 const Index = () => {
   const { data: session } = useSession();
+
+  const location = useRouter();
+
+  console.log(location);
+
   return (
     <div className='hidden border-r bg-muted/40 md:block'>
       <div className='flex h-full max-h-screen flex-col gap-2'>
         <div className='flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6'>
           <Link href='/' className='flex items-center gap-2 font-semibold'>
-            <Package2 className='h-6 w-6' />
-            <span className=''>Virtual Inventarios</span>
+            <Library className='h-6 w-6' />
+            <span className=''>BiblioGest</span>
           </Link>
           <Button variant='outline' size='icon' className='ml-auto h-8 w-8'>
             <Bell className='h-4 w-4' />
@@ -26,38 +50,58 @@ const Index = () => {
           <nav className='grid items-start px-2 text-sm font-medium lg:px-4'>
             <Link
               href='/'
-              className='flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary'
+              className={`flex items-center gap-3 rounded-lg px-3 py-2 transition-all ${
+                location.pathname === '/'
+                  ? 'bg-muted text-primary'
+                  : 'text-muted-foreground'
+              } hover:text-primary`}
             >
               <Home className='h-4 w-4' />
               Dashboard
             </Link>
             <Link
               href='/orders'
-              className='flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary'
+              className={`flex items-center gap-3 rounded-lg px-3 py-2 transition-all ${
+                location.pathname === '/orders'
+                  ? 'bg-muted text-primary'
+                  : 'text-muted-foreground'
+              } hover:text-primary`}
             >
-              <ShoppingCart className='h-4 w-4' />
-              Orders
+              <LibraryBig className='h-4 w-4' />
+              Requests
               <Badge className='ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full'>
                 6
               </Badge>
             </Link>
             <Link
               href='/products'
-              className='flex items-center gap-3 rounded-lg bg-muted px-3 py-2 text-primary transition-all hover:text-primary'
+              className={`flex items-center gap-3 rounded-lg px-3 py-2 transition-all ${
+                location.pathname === '/products'
+                  ? 'bg-muted text-primary'
+                  : 'text-muted-foreground'
+              } hover:text-primary`}
             >
-              <Package className='h-4 w-4' />
-              Products{' '}
+              <BookCopy className='h-4 w-4' />
+              Books{' '}
             </Link>
             <Link
               href='/customers'
-              className='flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary'
+              className={`flex items-center gap-3 rounded-lg px-3 py-2 transition-all ${
+                location.pathname === '/customers'
+                  ? 'bg-muted text-primary'
+                  : 'text-muted-foreground'
+              } hover:text-primary`}
             >
               <Users className='h-4 w-4' />
-              Customers
+              Clients
             </Link>
             <Link
               href='/users'
-              className='flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary'
+              className={`flex items-center gap-3 rounded-lg px-3 py-2 transition-all ${
+                location.pathname === '/users'
+                  ? 'bg-muted text-primary'
+                  : 'text-muted-foreground'
+              } hover:text-primary`}
             >
               <LineChart className='h-4 w-4' />
               Users
@@ -69,11 +113,12 @@ const Index = () => {
             <CardHeader className='flex flex-row gap-5 justify-center items-center p-2 pt-0 md:p-4'>
               <div>
                 <CardTitle>{session?.user?.name}</CardTitle>
-                <CardTitle>{session?.user?.email}</CardTitle>
                 <CardTitle>Admin</CardTitle>
               </div>
               <Avatar>
-                <AvatarImage src={session?.user?.image ?? 'https://github.com/shadcn.png'} />
+                <AvatarImage
+                  src={session?.user?.image ?? 'https://github.com/shadcn.png'}
+                />
                 <AvatarFallback>CN</AvatarFallback>
               </Avatar>
             </CardHeader>
